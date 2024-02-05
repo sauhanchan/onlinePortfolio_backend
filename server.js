7e8c002edd898f0b8484d7a2a6a1772f5d812346
SauHanChan <schan253@my.centennialcollege.ca>
2024-02-05 16:33:15 -0500

    CORSoptionsEdited-NoSlash

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ db.mongoose.connect(db.url, {
 //#set the origin then add cors middlewares using corsOptions as an argument
 var corsOptions = {
   //origin: "http://localhost:8081"
-  origin: "https://aesthetic-tulumba-4d6e98.netlify.app/"
+  origin: "https://aesthetic-tulumba-4d6e98.netlify.app"
 };
 app.use(cors(corsOptions));
 
